a14270414cfa5fa39c2fea078bbad1271cf5ce08
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2023-09-02 15:22:52 +0300

    abstaction added

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,22 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+    private final CommentRepository commentRepository; //we will not change the dependencies, so we put as a final
+    private final CommentNotificationProxy commentNotificationProxy;
+    //@Autowired since there is only one constructor, no need to put @Autowired
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
