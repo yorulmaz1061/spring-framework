d59ed9b051a1f6d749c3ddcf0a33901d2f135bd7
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2023-09-04 20:14:52 +0300

    @qualifier added

--- Spring-04-Abstraction/src/main/java/com/cydeo/config/ProjectConfig.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/config/ProjectConfig.java
@@ -3,7 +3,7 @@ package com.cydeo.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 @Configuration
-//@ComponentScan(basePackages = "com.cydeo")   //Both 2 ways is suitable
+//@ComponentScan(basePackages = "com.cydeo")   //Both 2 ways is suitable either "com.cydeo" or like below
 @ComponentScan(basePackages = {"com.cydeo.proxy","com.cydeo.service","com.cydeo.repository"})
 public class ProjectConfig {
 }
--- Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -3,18 +3,24 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@AllArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository; //we will not change the dependencies, so we put as a final
     private final CommentNotificationProxy commentNotificationProxy;
     //@Autowired since there is only one constructor, no need to put @Autowired
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    //we need to initialize dependencies so, we need to create constructor
+    //If you put @AllArgConstructor you don't need to create explicit constructor
+   public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
+    // This is our business logic.
     public void publishComment(Comment comment){
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
