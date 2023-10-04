60a4b3a16faf3bb0e6f71467b1d91d6582193585
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2023-10-04 15:42:08 +0300

    SpringThymeleaf

--- Spring-02-Core-Wiring/src/main/java/com/cydeo/ConfigCar.java
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/ConfigCar.java
@@ -27,5 +27,4 @@ public class ConfigCar {
        return p;
    }
 
-
 }
--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Controller
 public class StudentController {
